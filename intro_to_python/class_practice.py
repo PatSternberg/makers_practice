32d2574d33e124c54fd785c666de66fad67dc6b5
patrickskipworth <pskipworth@fastmail.com>
2024-01-18 15:03:22 +0000

    Set up class for containing candidate info, and function to print it

---
+++ intro_to_python/class_practice.py
@@ -0,0 +1,16 @@
+class candidate():
+    def __init__(self, firstName, surname, languages):
+        self.firstName = firstName
+        self.surname = surname
+        self.languages = languages    
+
+candidate1 = candidate('Vivec', 'Ald', 'C')
+candidate2 = candidate('Balmora', 'Sil', 'Python')
+candidate3 = candidate('Branora', 'Port', 'C#')
+
+def printDeets(n):
+    print(n.firstName + ' ' + n.surname + ', ' + n.languages)
+
+printDeets(candidate1)
+printDeets(candidate2)
+printDeets(candidate3)
